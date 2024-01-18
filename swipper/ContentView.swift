fc933be6759a16b435cac20a2766a785cea02e84
RobinK1234 <112863319+RobinK1234@users.noreply.github.com>
2024-01-18 11:32:35 +0200

    Update ContentView.swift

--- swipper/ContentView.swift
+++ swipper/ContentView.swift
@@ -1,24 +1,67 @@
-//
-//  ContentView.swift
-//  swipper
-//
-//  Created by Robin Kriisa on 18.01.2024.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [Color(hex: "00CF3A"), Color(hex: "0085FF")]), startPoint: .bottom, endPoint: .top)
+                    .edgesIgnoringSafeArea(.all)
+
+                VStack(spacing: 30) {
+                    
+                    Text("MINESWIIPER")
+                        .font(.system(size: 40, weight: .black))
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 40.0)
+                        .padding(.vertical, 20)
+                        .background(Color.green)
+                        .cornerRadius(25)
+                        .padding(.bottom, 100)
+
+                    // Play Game button
+                    NavigationLink(destination: GameView()) {
+                        Text("Play game")
+                            .font(.system(size: 30, weight: .bold))
+                            .foregroundColor(Color(hex: "00CF3A"))
+                            .frame(width: 300, height: 80)
+                            .background(Color.white)
+                            .cornerRadius(20)
+                    }
+                    .padding(.bottom, 20)
+
+                    // Help button
+                    NavigationLink(destination: HelpView()) {
+                        Text("Help")
+                            .font(.system(size: 30, weight: .bold))
+                            .foregroundColor(Color(hex: "0085FF"))
+                            .frame(width: 300, height: 80)
+                            .background(Color.white)
+                            .cornerRadius(20)
+                    }
+                }
+            }
+            .navigationBarHidden(true)
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+
+        let r = Double((rgbValue & 0xff0000) >> 16) / 255.0
+        let g = Double((rgbValue & 0x00ff00) >> 8) / 255.0
+        let b = Double(rgbValue & 0x0000ff) / 255.0
+        
+        self.init(red: r, green: g, blue: b)
+    }
 }
